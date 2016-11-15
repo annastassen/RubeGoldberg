a4a887c080f382bc56d85e76a0dec66e10995af4
Anna Stassen <as6644@nyu.ed>
2016-11-15 14:14:42 -0500

    Trigger camera and spacebar platform move

---
+++ RubeGoldberg/Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlatform : MonoBehaviour {
+
+	private Vector3 destination;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space)){
+
+			transform.position = new Vector3(transform.position.x, -3.0f, transform.position.z);
+
+		}
+
+	}
+}
+
