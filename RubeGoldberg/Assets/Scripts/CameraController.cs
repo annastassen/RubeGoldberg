4055b1989f257f5983344b30ed4269e958a1fe32
Anna Stassen <as6644@nyu.ed>
2016-11-15 03:18:29 -0500

    One camera working

--- RubeGoldberg/Assets/Scripts/CameraController.cs
+++ RubeGoldberg/Assets/Scripts/CameraController.cs
@@ -1,22 +1,24 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	//	public GameObject Camera1;
-	//	public GameObject Camera2;
+	//public GameObject Camera1;
+	//public GameObject Camera2;
 
 	public GameObject[] Cameras;
 	private int currentCam;
 
+
 	// Use this for initialization
 	void Start () {
-		// make camera 1 active by default
-		//		Camera1.SetActive (true);
-		//		Camera2.SetActive (false);
+		// make camera 1 active by default;
+		//Camera1.SetActive(true);
+		//Camera2.SetActive(false);
 
 		DeactivateCameras();
-		Cameras [0].SetActive (true);
+		Cameras[0].SetActive(true);
 		currentCam = 0;
 
 	}
@@ -24,35 +26,39 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			/* for swapping between two cameras
+			Camera1.SetActive(!Camera1.active);
+			Camera2.SetActive(!Camera2.active);
+			*/
 
-		if (Input.GetKeyDown (KeyCode.Space)) {
-
-			//			Camera1.SetActive (!Camera1.active);
-			//			Camera2.SetActive (!Camera2.active);
-
-			DeactivateCameras ();
+			DeactivateCameras();
 
 			if (currentCam < Cameras.Length - 1) {
-				currentCam += 1;
+				currentCam +=1;
 			} else {
 				currentCam = 0;
 			}
 
-			Cameras [currentCam].SetActive (true);
 
+			Cameras[currentCam].SetActive(true);
 
 		}
+
 	}
 
 	public void activateCamera(int _camNumber){
+
 		Cameras[_camNumber - 1].SetActive(true);
 		currentCam = _camNumber - 1;
 	}
 
-	public void DeactivateCameras(){
-
-		for (int i = 0; i < Cameras.Length; i++) {
-			Cameras [i].SetActive (false);
+	public void DeactivateCameras()
+	{
+		for (int i = 0; i < Cameras.Length; i++)
+		{
+			Cameras[i].SetActive(false);
 		}
 
 	}
--- RubeGoldberg/Assets/Scripts/CameraTrigger.cs
+++ RubeGoldberg/Assets/Scripts/CameraTrigger.cs
@@ -4,16 +4,14 @@ using System.Collections;
 public class CameraTrigger : MonoBehaviour {
 
 	//public GameObject camToActivate;
-
 	public int camNumber;
 
 	public CameraController controller;
 
 	void OnTriggerEnter(Collider other){
 
-		controller.DeactivateCameras ();
-
+		controller.DeactivateCameras();
+		controller.activateCamera(camNumber);
 
 	}
-
 }
