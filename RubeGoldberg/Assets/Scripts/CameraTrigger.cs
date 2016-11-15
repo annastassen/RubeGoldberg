bbf03837c9a35f4567fd182a000236f53b182a99
Anna Stassen <as6644@nyu.ed>
2016-11-15 02:25:11 -0500

    Added materials, skybox, and more objects

---
+++ RubeGoldberg/Assets/Scripts/CameraTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTrigger : MonoBehaviour {
+
+	//public GameObject camToActivate;
+
+	public int camNumber;
+
+	public CameraController controller;
+
+	void OnTriggerEnter(Collider other){
+
+		controller.DeactivateCameras ();
+
+
+	}
+
+}
